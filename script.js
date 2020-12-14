40bba5bec40d9a1853e473085e86c29bc69556d1
Jas James <jasdjames@gmail.com>
2020-12-14 00:42:43 -0600

    added charAt to array in script.js

--- script.js
+++ script.js
@@ -30,6 +30,7 @@ var passwordLowercase
 var passwordNumber
 var paswordSpecial 
 
+// Does not work as expected 
 
 // for (var i =0; i < passwordTypes.length; i++) {
 //   passwordTypes = prompt("If you would like to include " + passwordTypes[i] + " characters in your password type :" + passwordTypes[i++]);
@@ -45,18 +46,23 @@ var paswordSpecial
 
 var passwordUppercase = prompt("If you would like to include uppercase characters in your password type: uppercase. If not type: x ");
 passwordUppercase = passwordUppercase.toLowerCase();
+// most users will at least get the first letter of the spelling correct 
+passwordUppercase = passwordUppercase.charAt (0);
 passwordSelection.push(passwordUppercase);
 
 var passwordLowercase = prompt("If you would like to include lowercase characters in your password type: lowercase. If not type: x");
 passwordLowercase = passwordLowercase.toLowerCase();
+passwordLowercase = passwordLowercase.charAt (0);
 passwordSelection.push(passwordLowercase);
 
 var passwordNumber = prompt("If you would like to include numbers in your password type: number. If not type: x");
 passwordNumber = passwordNumber.toLowerCase();
+passwordNumber = passwordNumber.charAt (0);
 passwordSelection.push(passwordNumber);
 
 var passwordSpecial = prompt("If you would like to include uppercase characters in your password type: special. If not type: x");
 passwordSpecial = passwordSpecial.toLowerCase();
+passwordSpecial = passwordSpecial.charAt (0);
 passwordSelection.push(passwordSpecial);
 
 
